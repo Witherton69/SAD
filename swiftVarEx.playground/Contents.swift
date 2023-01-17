b9b7eac7f93b7a56416f990e49e0c0358f7ecd38
Witherton <katelyn.gosnell@stu.barren.kyschools.us>
2023-01-17 08:34:35 -0600

    Initial Commit

---
+++ swiftVarEx.playground/Contents.swift
@@ -0,0 +1,28 @@
+//1
+print("Witherton")
+
+//2
+//Int, String, Float, Double, Bool, Character
+
+//3
+//let
+
+//4
+var name = "Chris"
+
+//5
+name = "Robert"
+
+//BONUS
+
+//1
+var num = 3
+
+//2
+num = 3+2
+
+//3
+num+5;
+
+//4
+var nums = [1,8,9]
